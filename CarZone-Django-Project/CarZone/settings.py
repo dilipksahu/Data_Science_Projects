a72bdc4e0a34b942c27d036708aea949fdad9c53
Dilip <sahud048@gmail.com>
2020-10-05 21:41:36 +0530

    A pages app created and made change in setting,views,urls and also git ignore file created

--- CarZone-Django-Project/CarZone/settings.py
+++ CarZone-Django-Project/CarZone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'CarZone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
